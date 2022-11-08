04c615ba4ca73a0cdab4445d90a2136875cbc57b
phamquanglinhdev <phamquanglinhdev@gmail.com>
2022-11-08 10:16:18 +0700

    link regitser

--- src/main/java/com/adonis/haichanbank/utils/PhoneNotification.java
+++ src/main/java/com/adonis/haichanbank/utils/PhoneNotification.java
@@ -9,10 +9,14 @@ import static com.slack.api.webhook.WebhookPayloads.*;
 
 public class PhoneNotification {
     public void make(String phone, String notification) throws IOException {
-        String webhookUrl = "https://hooks.slack.com/services/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";
+        String webhookUrl = "https://hooks.slack.com/services/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";
         Slack slack = Slack.getInstance();
         WebhookResponse response = slack.send(webhookUrl, payload(
-                p -> p.text(phone + ":" + notification)
+                p -> p
+                        .text(notification)
+                        .username(phone)
+                        .iconUrl("https://haycafe.vn/wp-content/uploads/2022/02/Anh-gai-xinh-Viet-Nam.jpg")
+
         ));
     }
 }
